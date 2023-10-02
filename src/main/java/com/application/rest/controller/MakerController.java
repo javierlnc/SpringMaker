5a093d78b8d4871d06640914ee34448debb9f936
javierlnc <javierlnc@icloud.com>
2023-10-01 20:07:59 -0500

    Se crean metoods del MakerController

--- src/main/java/com/application/rest/controller/MakerController.java
+++ src/main/java/com/application/rest/controller/MakerController.java
@@ -5,10 +5,12 @@ import com.application.rest.entities.Maker;
 import com.application.rest.service.IMakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -32,5 +34,43 @@ public class MakerController {
         return ResponseEntity.notFound().build();
 
     }
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<MakerDTO> makerDTOList = makerService.findAll()
+                .stream()
+                .map(maker -> MakerDTO.builder()
+                        .id(maker.getId())
+                        .name(maker.getName())
+                        .productList(maker.getProductList())
+                        .build())
+                .toList();
 
+        return  ResponseEntity.ok(makerDTOList);
+    }
+    @PostMapping("/Save")
+    public ResponseEntity<?> save(@RequestBody MakerDTO makerDTO) throws URISyntaxException {
+        try {
+            makerService.save(Maker.builder().name(makerDTO.getName()).build());
+            return ResponseEntity.created(new URI("/api/maker/save")).build();
+
+        }catch (InputMismatchException ex){
+            System.out.println("Debe ingresar un nombre");
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+        try {
+            Optional<Maker> makerOptional = makerService.findById(id);
+            Maker maker = makerOptional.get();
+            maker.setName(makerDTO.getName());
+            makerService.save(maker);
+            return ResponseEntity.ok("Registro Actualizado");
+
+        }catch (Exception ex){
+            return ResponseEntity.notFound().build();
+
+        }
+    }
 }
